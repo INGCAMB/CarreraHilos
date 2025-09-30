f74f65769ae1ba5b2954bc7e36166490bd8eb793
Albertombn_0 <carlosmbn007@gmail.com>
2025-09-30 15:55:14 -0700

    Agregar archivos de proyecto.

---
+++ CarreraHilos/Carrera.Designer.cs
@@ -0,0 +1,369 @@
+﻿namespace CarreraHilos
+{
+    partial class frmCarrera
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCarrera));
+            this.btnReiniciar = new System.Windows.Forms.Button();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.lblCaballoN = new System.Windows.Forms.Label();
+            this.pnlMenu = new System.Windows.Forms.Panel();
+            this.lblGana4 = new System.Windows.Forms.Label();
+            this.lblGana3 = new System.Windows.Forms.Label();
+            this.lblGana2 = new System.Windows.Forms.Label();
+            this.lblGana1 = new System.Windows.Forms.Label();
+            this.pbPortada2 = new System.Windows.Forms.PictureBox();
+            this.pbPortada1 = new System.Windows.Forms.PictureBox();
+            this.lblCaballoC = new System.Windows.Forms.Label();
+            this.lblCaballoG = new System.Windows.Forms.Label();
+            this.lblCaballoA = new System.Windows.Forms.Label();
+            this.pbCaballoAmarillo = new System.Windows.Forms.PictureBox();
+            this.pbCaballoGris = new System.Windows.Forms.PictureBox();
+            this.pbCaballoCafe = new System.Windows.Forms.PictureBox();
+            this.pbCaballoNegro = new System.Windows.Forms.PictureBox();
+            this.pbStart = new System.Windows.Forms.PictureBox();
+            this.pbMeta = new System.Windows.Forms.PictureBox();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.pnlMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPortada2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPortada1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCaballoAmarillo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCaballoGris)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCaballoCafe)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCaballoNegro)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbStart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMeta)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnReiniciar
+            // 
+            this.btnReiniciar.BackColor = System.Drawing.Color.DarkGoldenrod;
+            this.btnReiniciar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnReiniciar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReiniciar.ForeColor = System.Drawing.Color.White;
+            this.btnReiniciar.Location = new System.Drawing.Point(551, 177);
+            this.btnReiniciar.Margin = new System.Windows.Forms.Padding(0);
+            this.btnReiniciar.Name = "btnReiniciar";
+            this.btnReiniciar.Size = new System.Drawing.Size(98, 54);
+            this.btnReiniciar.TabIndex = 53;
+            this.btnReiniciar.Text = "Reiniciar carrera";
+            this.btnReiniciar.UseVisualStyleBackColor = false;
+            this.btnReiniciar.Click += new System.EventHandler(this.btnReiniciar_Click);
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.BackColor = System.Drawing.Color.DarkGoldenrod;
+            this.btnIniciar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnIniciar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIniciar.ForeColor = System.Drawing.Color.White;
+            this.btnIniciar.Location = new System.Drawing.Point(360, 177);
+            this.btnIniciar.Margin = new System.Windows.Forms.Padding(0);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(98, 54);
+            this.btnIniciar.TabIndex = 50;
+            this.btnIniciar.Text = "Iniciar carrera";
+            this.btnIniciar.UseVisualStyleBackColor = false;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // lblCaballoN
+            // 
+            this.lblCaballoN.AutoSize = true;
+            this.lblCaballoN.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCaballoN.ForeColor = System.Drawing.Color.Black;
+            this.lblCaballoN.Location = new System.Drawing.Point(293, 33);
+            this.lblCaballoN.Name = "lblCaballoN";
+            this.lblCaballoN.Size = new System.Drawing.Size(122, 20);
+            this.lblCaballoN.TabIndex = 42;
+            this.lblCaballoN.Text = "Caballo Negro";
+            // 
+            // pnlMenu
+            // 
+            this.pnlMenu.BackColor = System.Drawing.Color.DarkGray;
+            this.pnlMenu.Controls.Add(this.lblGana4);
+            this.pnlMenu.Controls.Add(this.lblGana3);
+            this.pnlMenu.Controls.Add(this.lblGana2);
+            this.pnlMenu.Controls.Add(this.lblGana1);
+            this.pnlMenu.Controls.Add(this.pbPortada2);
+            this.pnlMenu.Controls.Add(this.pbPortada1);
+            this.pnlMenu.Controls.Add(this.lblCaballoN);
+            this.pnlMenu.Controls.Add(this.lblCaballoC);
+            this.pnlMenu.Controls.Add(this.lblCaballoG);
+            this.pnlMenu.Controls.Add(this.lblCaballoA);
+            this.pnlMenu.Controls.Add(this.btnIniciar);
+            this.pnlMenu.Controls.Add(this.btnReiniciar);
+            this.pnlMenu.Location = new System.Drawing.Point(0, 242);
+            this.pnlMenu.Name = "pnlMenu";
+            this.pnlMenu.Size = new System.Drawing.Size(1013, 258);
+            this.pnlMenu.TabIndex = 68;
+            // 
+            // lblGana4
+            // 
+            this.lblGana4.AutoSize = true;
+            this.lblGana4.Location = new System.Drawing.Point(675, 114);
+            this.lblGana4.Name = "lblGana4";
+            this.lblGana4.Size = new System.Drawing.Size(38, 13);
+            this.lblGana4.TabIndex = 71;
+            this.lblGana4.Text = "Suerte";
+            // 
+            // lblGana3
+            // 
+            this.lblGana3.AutoSize = true;
+            this.lblGana3.Location = new System.Drawing.Point(675, 38);
+            this.lblGana3.Name = "lblGana3";
+            this.lblGana3.Size = new System.Drawing.Size(38, 13);
+            this.lblGana3.TabIndex = 70;
+            this.lblGana3.Text = "Suerte";
+            // 
+            // lblGana2
+            // 
+            this.lblGana2.AutoSize = true;
+            this.lblGana2.Location = new System.Drawing.Point(436, 114);
+            this.lblGana2.Name = "lblGana2";
+            this.lblGana2.Size = new System.Drawing.Size(38, 13);
+            this.lblGana2.TabIndex = 69;
+            this.lblGana2.Text = "Suerte";
+            // 
+            // lblGana1
+            // 
+            this.lblGana1.AutoSize = true;
+            this.lblGana1.Location = new System.Drawing.Point(436, 38);
+            this.lblGana1.Name = "lblGana1";
+            this.lblGana1.Size = new System.Drawing.Size(38, 13);
+            this.lblGana1.TabIndex = 68;
+            this.lblGana1.Text = "Suerte";
+            // 
+            // pbPortada2
+            // 
+            this.pbPortada2.BackgroundImage = global::CarreraHilos.Properties.Resources.Portada2;
+            this.pbPortada2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pbPortada2.Location = new System.Drawing.Point(747, 0);
+            this.pbPortada2.Name = "pbPortada2";
+            this.pbPortada2.Size = new System.Drawing.Size(266, 255);
+            this.pbPortada2.TabIndex = 67;
+            this.pbPortada2.TabStop = false;
+            // 
+            // pbPortada1
+            // 
+            this.pbPortada1.BackgroundImage = global::CarreraHilos.Properties.Resources.Portada1;
+            this.pbPortada1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pbPortada1.Location = new System.Drawing.Point(0, 0);
+            this.pbPortada1.Name = "pbPortada1";
+            this.pbPortada1.Size = new System.Drawing.Size(266, 255);
+            this.pbPortada1.TabIndex = 66;
+            this.pbPortada1.TabStop = false;
+            // 
+            // lblCaballoC
+            // 
+            this.lblCaballoC.AutoSize = true;
+            this.lblCaballoC.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCaballoC.ForeColor = System.Drawing.Color.Black;
+            this.lblCaballoC.Location = new System.Drawing.Point(293, 109);
+            this.lblCaballoC.Name = "lblCaballoC";
+            this.lblCaballoC.Size = new System.Drawing.Size(112, 20);
+            this.lblCaballoC.TabIndex = 43;
+            this.lblCaballoC.Text = "Caballo Cafe";
+            // 
+            // lblCaballoG
+            // 
+            this.lblCaballoG.AutoSize = true;
+            this.lblCaballoG.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCaballoG.ForeColor = System.Drawing.Color.Black;
+            this.lblCaballoG.Location = new System.Drawing.Point(521, 33);
+            this.lblCaballoG.Name = "lblCaballoG";
+            this.lblCaballoG.Size = new System.Drawing.Size(107, 20);
+            this.lblCaballoG.TabIndex = 44;
+            this.lblCaballoG.Text = "Caballo Gris";
+            // 
+            // lblCaballoA
+            // 
+            this.lblCaballoA.AutoSize = true;
+            this.lblCaballoA.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCaballoA.ForeColor = System.Drawing.Color.Black;
+            this.lblCaballoA.Location = new System.Drawing.Point(521, 109);
+            this.lblCaballoA.Name = "lblCaballoA";
+            this.lblCaballoA.Size = new System.Drawing.Size(138, 20);
+            this.lblCaballoA.TabIndex = 45;
+            this.lblCaballoA.Text = "Caballo Amarillo";
+            // 
+            // pbCaballoAmarillo
+            // 
+            this.pbCaballoAmarillo.BackgroundImage = global::CarreraHilos.Properties.Resources.CaballoAmarillo;
+            this.pbCaballoAmarillo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pbCaballoAmarillo.Location = new System.Drawing.Point(12, 180);
+            this.pbCaballoAmarillo.Name = "pbCaballoAmarillo";
+            this.pbCaballoAmarillo.Size = new System.Drawing.Size(64, 50);
+            this.pbCaballoAmarillo.TabIndex = 72;
+            this.pbCaballoAmarillo.TabStop = false;
+            // 
+            // pbCaballoGris
+            // 
+            this.pbCaballoGris.BackgroundImage = global::CarreraHilos.Properties.Resources.CaballoGris;
+            this.pbCaballoGris.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pbCaballoGris.Location = new System.Drawing.Point(12, 124);
+            this.pbCaballoGris.Name = "pbCaballoGris";
+            this.pbCaballoGris.Size = new System.Drawing.Size(64, 50);
+            this.pbCaballoGris.TabIndex = 71;
+            this.pbCaballoGris.TabStop = false;
+            // 
+            // pbCaballoCafe
+            // 
+            this.pbCaballoCafe.BackgroundImage = global::CarreraHilos.Properties.Resources.CaballoCafe;
+            this.pbCaballoCafe.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pbCaballoCafe.Location = new System.Drawing.Point(12, 68);
+            this.pbCaballoCafe.Name = "pbCaballoCafe";
+            this.pbCaballoCafe.Size = new System.Drawing.Size(64, 50);
+            this.pbCaballoCafe.TabIndex = 70;
+            this.pbCaballoCafe.TabStop = false;
+            // 
+            // pbCaballoNegro
+            // 
+            this.pbCaballoNegro.BackgroundImage = global::CarreraHilos.Properties.Resources.CaballoNegro;
+            this.pbCaballoNegro.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pbCaballoNegro.Location = new System.Drawing.Point(12, 12);
+            this.pbCaballoNegro.Name = "pbCaballoNegro";
+            this.pbCaballoNegro.Size = new System.Drawing.Size(64, 50);
+            this.pbCaballoNegro.TabIndex = 69;
+            this.pbCaballoNegro.TabStop = false;
+            // 
+            // pbStart
+            // 
+            this.pbStart.BackgroundImage = global::CarreraHilos.Properties.Resources.Salida;
+            this.pbStart.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pbStart.Location = new System.Drawing.Point(0, 0);
+            this.pbStart.Margin = new System.Windows.Forms.Padding(0);
+            this.pbStart.Name = "pbStart";
+            this.pbStart.Size = new System.Drawing.Size(86, 242);
+            this.pbStart.TabIndex = 67;
+            this.pbStart.TabStop = false;
+            // 
+            // pbMeta
+            // 
+            this.pbMeta.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pbMeta.BackgroundImage")));
+            this.pbMeta.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pbMeta.Location = new System.Drawing.Point(944, 0);
+            this.pbMeta.Margin = new System.Windows.Forms.Padding(0);
+            this.pbMeta.Name = "pbMeta";
+            this.pbMeta.Size = new System.Drawing.Size(69, 242);
+            this.pbMeta.TabIndex = 66;
+            this.pbMeta.TabStop = false;
+            // 
+            // lbl1
+            // 
+            this.lbl1.BackColor = System.Drawing.Color.Transparent;
+            this.lbl1.ForeColor = System.Drawing.Color.White;
+            this.lbl1.Location = new System.Drawing.Point(86, 58);
+            this.lbl1.Margin = new System.Windows.Forms.Padding(0);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(858, 15);
+            this.lbl1.TabIndex = 73;
+            this.lbl1.Text = resources.GetString("lbl1.Text");
+            // 
+            // lbl2
+            // 
+            this.lbl2.BackColor = System.Drawing.Color.Transparent;
+            this.lbl2.ForeColor = System.Drawing.Color.White;
+            this.lbl2.Location = new System.Drawing.Point(86, 114);
+            this.lbl2.Margin = new System.Windows.Forms.Padding(0);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(858, 15);
+            this.lbl2.TabIndex = 74;
+            this.lbl2.Text = resources.GetString("lbl2.Text");
+            // 
+            // lbl3
+            // 
+            this.lbl3.BackColor = System.Drawing.Color.Transparent;
+            this.lbl3.ForeColor = System.Drawing.Color.White;
+            this.lbl3.Location = new System.Drawing.Point(86, 171);
+            this.lbl3.Margin = new System.Windows.Forms.Padding(0);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(858, 15);
+            this.lbl3.TabIndex = 75;
+            this.lbl3.Text = resources.GetString("lbl3.Text");
+            // 
+            // frmCarrera
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGoldenrod;
+            this.ClientSize = new System.Drawing.Size(1012, 501);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.pbCaballoAmarillo);
+            this.Controls.Add(this.pbCaballoGris);
+            this.Controls.Add(this.pbCaballoCafe);
+            this.Controls.Add(this.pbCaballoNegro);
+            this.Controls.Add(this.pnlMenu);
+            this.Controls.Add(this.pbStart);
+            this.Controls.Add(this.pbMeta);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "frmCarrera";
+            this.Text = "Carrera de caballos";
+            this.Load += new System.EventHandler(this.frmCarrera_Load);
+            this.pnlMenu.ResumeLayout(false);
+            this.pnlMenu.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPortada2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPortada1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCaballoAmarillo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCaballoGris)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCaballoCafe)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCaballoNegro)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbStart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMeta)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnReiniciar;
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.Label lblCaballoN;
+        private System.Windows.Forms.PictureBox pbMeta;
+        private System.Windows.Forms.PictureBox pbStart;
+        private System.Windows.Forms.Panel pnlMenu;
+        private System.Windows.Forms.PictureBox pbPortada1;
+        private System.Windows.Forms.PictureBox pbPortada2;
+        private System.Windows.Forms.PictureBox pbCaballoNegro;
+        private System.Windows.Forms.PictureBox pbCaballoCafe;
+        private System.Windows.Forms.PictureBox pbCaballoGris;
+        private System.Windows.Forms.PictureBox pbCaballoAmarillo;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Label lblCaballoC;
+        private System.Windows.Forms.Label lblCaballoG;
+        private System.Windows.Forms.Label lblCaballoA;
+        private System.Windows.Forms.Label lblGana1;
+        private System.Windows.Forms.Label lblGana4;
+        private System.Windows.Forms.Label lblGana3;
+        private System.Windows.Forms.Label lblGana2;
+    }
+}
+
---
+++ CarreraHilos/Carrera.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//Para manejar hilos
+using System.Threading;
+
+namespace CarreraHilos
+{
+    public partial class frmCarrera : Form
+    {
+        //Variables utilizadas para velocidad
+        int A, B, C, D;
+
+        //Definir diferentes procesos
+        Thread procesodecaballo;
+
+        //Random para variar los cambios
+        Random random = new Random();
+
+        //Variables
+        //int Contador = 0;
+
+        public frmCarrera()
+        {
+            InitializeComponent();
+
+            procesodecaballo = new Thread(new ThreadStart(Movimiento));
+
+            btnReiniciar.Enabled = false;
+        }
+
+        private void frmCarrera_Load(object sender, EventArgs e)
+        {
+            //Esta linea te permite ejecutar varios hilos
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        public void Movimiento()
+        {
+            while (true)
+            {
+                A = random.Next(1, 10);
+                B = random.Next(1, 10);
+                C = random.Next(1, 10);
+                D = random.Next(1, 10);
+
+                if (pbCaballoNegro.Right >= pbMeta.Left)
+                {
+                    lblGana1.Text = "Gana";
+                    lblGana2.Text = "Pierde";
+                    lblGana3.Text = "Pierde";
+                    lblGana4.Text = "Pierde";
+                    lblGana1.ForeColor = Color.Green;
+                    lblGana2.ForeColor = Color.Red;
+                    lblGana3.ForeColor = Color.Red;
+                    lblGana4.ForeColor = Color.Red;
+                    procesodecaballo.Abort();
+                }
+                else if (pbCaballoCafe.Right >= pbMeta.Left)
+                {
+                    lblGana1.Text = "Pierde";
+                    lblGana2.Text = "Gana";
+                    lblGana3.Text = "Pierde";
+                    lblGana4.Text = "Pierde";
+                    lblGana1.ForeColor = Color.Red;
+                    lblGana2.ForeColor = Color.Green;
+                    lblGana3.ForeColor = Color.Red;
+                    lblGana4.ForeColor = Color.Red;
+                    procesodecaballo.Abort();
+                }
+                else if (pbCaballoGris.Right >= pbMeta.Left)
+                {
+                    lblGana1.Text = "Pierde";
+                    lblGana2.Text = "Pierde";
+                    lblGana3.Text = "Gana";
+                    lblGana4.Text = "Pierde";
+                    lblGana1.ForeColor = Color.Red;
+                    lblGana2.ForeColor = Color.Red;
+                    lblGana3.ForeColor = Color.Green;
+                    lblGana4.ForeColor = Color.Red;
+                    procesodecaballo.Abort();
+                }
+                else if (pbCaballoAmarillo.Right >= pbMeta.Left)
+                {
+                    lblGana1.Text = "Pierde";
+                    lblGana2.Text = "Pierde";
+                    lblGana3.Text = "Pierde";
+                    lblGana4.Text = "Gana";
+                    lblGana1.ForeColor = Color.Red;
+                    lblGana2.ForeColor = Color.Red;
+                    lblGana3.ForeColor = Color.Red;
+                    lblGana4.ForeColor = Color.Green;
+                    procesodecaballo.Abort();
+                }
+                else
+                {
+                    pbCaballoNegro.Left += A;
+                    pbCaballoCafe.Left += B;
+                    pbCaballoGris.Left += C;
+                    pbCaballoAmarillo.Left += D;
+                }
+
+                Thread.Sleep(50);
+            }
+        }
+
+        //Iniciar
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            btnIniciar.Enabled = false;
+            btnReiniciar.Enabled = true;
+
+            pbCaballoNegro.Left = +12;
+            pbCaballoCafe.Left = +12;
+            pbCaballoGris.Left = +12;
+            pbCaballoAmarillo.Left = +12;
+
+            try
+            {
+                procesodecaballo.Start();
+            }
+            catch (Exception d)
+            {
+                MessageBox.Show(d.Message);
+            }
+        }
+
+        //Abortar
+        private void btnReiniciar_Click(object sender, EventArgs e)
+        {
+            btnIniciar.Enabled = true;
+            btnReiniciar.Enabled = false;
+            lblGana1.Text = "Suerte";
+            lblGana1.ForeColor = Color.Black;
+            lblGana2.Text = "Suerte";
+            lblGana2.ForeColor = Color.Black;
+            lblGana3.Text = "Suerte";
+            lblGana3.ForeColor = Color.Black;
+            lblGana4.Text = "Suerte";
+            lblGana4.ForeColor = Color.Black;
+
+            try
+            {
+                if(procesodecaballo.IsAlive)
+                procesodecaballo.Abort();
+                procesodecaballo = new Thread(new ThreadStart(Movimiento));
+
+                pbCaballoNegro.Location = new Point(12, 12);
+                pbCaballoCafe.Location = new Point(12, 68);
+                pbCaballoGris.Location = new Point(12, 124);
+                pbCaballoAmarillo.Location = new Point(12, 180);
+            }
+            catch (Exception d)
+            {
+                MessageBox.Show(d.Message);
+            }
+        }
+    }
+}
